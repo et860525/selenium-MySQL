90502166056ea7012f7c0b2e0faa49d7af7a2077
et860525 <et860525@gmail.com>
2020-11-17 16:44:21 +0800

    visualization add csv direct show data, change yahoo-AAPL file name

--- yahoo-finance/csv-to-db/visualization.py
+++ yahoo-finance/csv-to-db/visualization.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
+
 import pandas as pd
 
 import read_db
@@ -9,21 +10,53 @@ import read_db
 date = []
 close = []
 
-data = read_db.get_data()
-try:
-    #df = pd.DataFrame(data=data, columns=['date','open','high','low','close','adj_close','volume'])
+#data = read_db.get_data()
+data = pd.read_csv('AAPL.csv')
+#df = pd.DataFrame(data=data, columns=['date','open','high','low','close','adj_close','volume'])
 
-    for row in data:
-        date.append(matplotlib.dates.date2num(datetime.strptime(row[0], "%Y-%m-%d")))
-        close.append(row[4])
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(matplotlib.dates.MonthLocator())
-    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m'))
-    #ax.tick_params(pad=20)
+def data_from_csv():
+    try:
+        for row in data['Date']:
+            # matplotlib.dates.date2num(): Convert datetime objects to Matplotlib dates.
+            # datetime.strptime(row[0], "%Y-%m-%d"): Translate string to datetime object.
+            date.append(matplotlib.dates.date2num(datetime.strptime(row, "%Y-%m-%d")))
+        
+        for row in data['Close']:
+            close.append(row)
 
-    plt.plot(date, close, linewidth=0.5)
-    plt.show()   
+        ax = plt.gca() # Get axes and draw (獲得軸線可進行繪畫)
+
+        ax.xaxis.set_major_locator(matplotlib.dates.MonthLocator()) # Find Month set main xaxis
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m')) # Set fomat for xaxis
+
+        plt.plot(date, close, linewidth=1)
+        plt.show()   
+    except:
+        pass
 
 
-except:
-    pass
+def data_from_db():
+    try:
+        for row in data:
+            # matplotlib.dates.date2num(): Convert datetime objects to Matplotlib dates.
+            # datetime.strptime(row[0], "%Y-%m-%d"): Translate string to datetime object.
+            date.append(matplotlib.dates.date2num(datetime.strptime(row[0], "%Y-%m-%d")))
+            close.append(row[4])
+
+        ax = plt.gca() # Get axes and draw (獲得軸線可進行繪畫)
+        
+        # Test 軸線繪畫，將上面的line與右邊的line，設為隱藏
+        #ax.spines['right'].set_color('none')
+        #ax.spines['top'].set_color('none')
+    
+        ax.xaxis.set_major_locator(matplotlib.dates.MonthLocator()) # Find Month set main xaxis
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m')) # Set fomat for xaxis
+
+        plt.plot(date, close, linewidth=1)
+        plt.show()   
+
+    except:
+        pass
+
+if __name__ == "__main__":
+    data_from_csv()
